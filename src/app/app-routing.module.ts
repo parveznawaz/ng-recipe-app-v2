c12b543b7819e6268060a707c0d4e56e67944afe
parveznawaz <parvez.nawaz@gmail.com>
2019-03-26 07:41:07 -0400

    Adding Editing Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes : Routes = [
   {
@@ -17,9 +18,18 @@ const routes : Routes = [
       {
         path: '',
         component: RecipeStartComponent
-      }, {
+      },
+      {
+        path: 'new',
+        component: RecipeEditComponent
+      },
+      {
         path: ':id',
         component: RecipeDetailComponent
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent
       }
     ]
   }, {
