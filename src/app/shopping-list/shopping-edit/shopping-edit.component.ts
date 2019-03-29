1b73075ba3e14b24c6f1971424001c84f1c733d4
Parvez Nawaz <parvez.nawaz@gmail.com>
2019-03-29 06:37:35 -0400

    replacing with ngForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,20 +8,19 @@ import {
 } from '@angular/core';
 import {Ingredient} from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
 
 @Component({selector: 'app-shopping-edit', templateUrl: './shopping-edit.component.html', styleUrls: ['./shopping-edit.component.css']})
 export class ShoppingEditComponent implements OnInit {
-  //@Output()ingredientAdded = new EventEmitter < Ingredient > ();
-  @ViewChild('nameInput')nameInputRef : ElementRef;
-  @ViewChild('amountInput')amountInputRef : ElementRef;
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {}
 
-  onAddItem() {
-    this
-      .shoppingListService
-      .addIngredient(new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value));
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 
 }
