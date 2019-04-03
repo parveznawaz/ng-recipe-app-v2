7d503d22416019e827b6f464b428cbc0f82c7ef9
parveznawaz <parvez.nawaz@gmail.com>
2019-04-03 14:43:42 -0400

    reacforing services to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,6 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuardService } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -28,7 +23,6 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [RecipeService,ShoppingListService,DataStorageService,AuthService,AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { AuthService } from '../auth/auth.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
 
 @NgModule({
     declarations: [
@@ -12,6 +16,11 @@ import { AppRoutingModule } from '../app-routing.module';
     imports: [
         SharedModule, AppRoutingModule
     ],
-    exports: [AppRoutingModule, HeaderComponent]
+    exports: [AppRoutingModule, HeaderComponent],
+    providers: [
+        RecipeService,
+        ShoppingListService,
+        DataStorageService,
+        AuthService]
 })
 export class CoreModule {}
