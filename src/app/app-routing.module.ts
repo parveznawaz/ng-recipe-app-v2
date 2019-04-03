d6ae01cee84ee45f5793c2ac28e787be08617551
parveznawaz <parvez.nawaz@gmail.com>
2019-04-03 15:14:28 -0400

    Preload strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { HomeComponent } from './core/home/home.component';
@@ -11,7 +11,7 @@ const routes : Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
